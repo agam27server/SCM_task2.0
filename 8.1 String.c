8012e23a4cc2b1c339ba913990399b543ee6a3dc
Gaurav_Singh_Heer <gauravheer2005@gmail.com>
2024-04-22 14:52:35 +0530

    new

---
+++ 8.1 String.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    
+    char name1[]={'S','H','R','A','D','H','A','\0'};
+    char name2[]={"SHRADHA"};
+    printf("%s\n",name1);
+    printf("%s\n",name2);
+}
---
+++ 8.2.1 String Iterate.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+void print(char name[]){
+    for (int i=0;i<=strlen(name);i++){
+        printf("%c\n",name[i]);
+    }
+}
+
+int main(){
+    char firstName[]="Shradha";
+    char lastName[]="Khapra";
+    printf("%c\n",firstName[0]);
+    printf("%c\n",firstName[6]);
+    printf("%c\n",firstName[7]);
+    printf("Now By Loop:)\n\n");
+    print(firstName);
+    print(lastName);
+}
